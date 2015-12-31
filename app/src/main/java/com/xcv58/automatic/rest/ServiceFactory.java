c536303c44abb8bd7ba789d5c0d7081c72e921cd
xcv58 <chenyihonglove@gmail.com>
2015-12-30 22:38:24 -0700

    add 5 seconds timeout for http requests

--- app/src/main/java/com/xcv58/automatic/rest/ServiceFactory.java
+++ app/src/main/java/com/xcv58/automatic/rest/ServiceFactory.java
@@ -1,5 +1,9 @@
 package com.xcv58.automatic.rest;
 
+import com.squareup.okhttp.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
+
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 import retrofit.RxJavaCallAdapterFactory;
@@ -9,8 +13,11 @@ import retrofit.RxJavaCallAdapterFactory;
  */
 public class ServiceFactory {
     public static <T> T createRetrofitService(final Class<T> clazz, final String endPoint) {
+        OkHttpClient client = new OkHttpClient();
+        client.setConnectTimeout(5000, TimeUnit.MILLISECONDS);
         final Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(endPoint)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
